107ce0415061757b02e6c1bd61d6f78e0942dd7e
Tyler McMaster <tyler@tlm13.com>
2023-03-20 16:42:12 -0400

    Bump version: 0.0.6 → 0.0.7

--- src/service_days/__init__.py
+++ src/service_days/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Tyler McMaster <tyler@tlm13.com>
 #
 # SPDX-License-Identifier: MIT
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 import functools
 import logging
